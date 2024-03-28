1bfcd922d99566407f8ade3001c74d072b830f05
Mike Katholnig <mk3819@gmail.com>
2024-03-28 11:04:53 +0000

    Tinder Swipe Animation

---
+++ src/app/(days)/day6/index.tsx
@@ -0,0 +1,23 @@
+import MarkdownDisplay from "@components/day3/MarkdownDisplay";
+import { Link, Stack } from "expo-router";
+import { Button } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const description = `
+  # Tinder Swipe Animation
+
+  Let's build the Tinder Swipe Animation in React Native using Reanimated
+`;
+
+export default function DayDetailsScreen() {
+  return (
+    <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
+      <Stack.Screen options={{ title: "Day 6: Tinder" }} />
+      <MarkdownDisplay>{description}</MarkdownDisplay>
+
+      <Link href={"/day6/tinder"} asChild>
+        <Button title="Go to Tinder" />
+      </Link>
+    </SafeAreaView>
+  );
+}
---
+++ src/app/(days)/day6/tinder.tsx
@@ -0,0 +1,63 @@
+import { View, StyleSheet, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import TinderCard from "@/components/day6/TinderCard";
+import { Stack } from "expo-router";
+import {
+  runOnJS,
+  useAnimatedReaction,
+  useSharedValue,
+} from "react-native-reanimated";
+import dummyUsers from "@assets/data/day6/users.json";
+
+export default function TinderScreen() {
+  const activeIndex = useSharedValue(0);
+  const [index, setIndex] = useState(0);
+  const [users, setUsers] = useState(dummyUsers);
+
+  useAnimatedReaction(
+    () => activeIndex.value,
+    (value, prevValue) => {
+      if (Math.floor(value) !== index) {
+        runOnJS(setIndex)(Math.floor(value));
+      }
+    }
+  );
+
+  useEffect(() => {
+    if (index > users.length - 3) {
+      setUsers((usrs) => [...usrs, ...dummyUsers.reverse()]);
+    }
+  }, [index]);
+
+  const onResponse = (res: boolean) => {
+    console.log("on Response", res);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ headerShown: false }} />
+      <Text style={{ position: "absolute", bottom: 20 }}>
+        Current index: {index}
+      </Text>
+
+      {users.map((user, index) => (
+        <TinderCard
+          key={`${user.id}-${index}`}
+          user={user}
+          numOfCards={users.length}
+          index={index}
+          activeIndex={activeIndex}
+          onResponse={onResponse}
+        />
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
